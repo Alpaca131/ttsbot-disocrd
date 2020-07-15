1556fdf0021a106a4a29646edb0c88aaa97eab63
BuildTools <unconfigured@null.spigotmc.org>
2020-07-15 11:06:22 +0900

    expand

--- bot.py
+++ bot.py
@@ -6,10 +6,17 @@ import os
 from dispander import dispand
 from discord.ext import tasks
 import re
+import json
+from urllib.request import *
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 client = discord.Client()
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 str_api_key = os.environ['GCP_API']
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 voice_active_guild = []
 lang = {}
 spk_rate_dic = {}
@@ -18,30 +25,78 @@ voice_active_ch = []
 
 @client.event
 async def on_ready():
+    global expand_off
     print('ready')
     await client.change_presence(activity=discord.Game(name="「t.help」でヘルプ", type=1))
+    f = drive.CreateFile({'id': '1zX-mbDeN_Mlx-p_62WSE5zAgsqu_jFX5'})
+    f.GetContentFile('expand.json')
+    with open('expand.json') as f:
+        expand_off = json.load(f)
     if not discord.opus.is_loaded():
         # もし未ロードだったら
         discord.opus.load_opus("heroku-buildpack-libopus")
 
 @client.event
 async def on_message(message):
-    global voice_active, dispand, spk_rate_dic
+    global voice_active, dispand, spk_rate_dic, expand_off
     if message.author.bot:
         return
+    if message.guild.id not in expand_off:
+        await dispand(message)
     if message.content == 't.help':
-        await message.channel.send('このBotのヘルプです。\n\n**「t.con (オプション:反応する対象、言語)」**\n(使用例：t.con en server)\n自分が接続しているVCにBotを接続させます。\n反応する対象：\n・指定なし(もしくはchannel)･･･コマンドのチャンネルに反応\n・server･･･サーバー全体に反応\n\n言語：\n・指定なし(もしくはjp)･･･日本語\n・en･･･英語\n・kr･･･韓国語\n・ch･･･中国語\n\n**「t.dc」**\n自分が接続しているVCからこのBotを切断します。\n\n**「t.release note」**\nこのBotの最新のアップデート内容を確認できます。\n\n**「t.invite」**\nこのBotの招待リンクを送ります。ご自由にお使い下さい。\n\n**「t.support」**\nこのBotのサポートサーバーの招待リンクを送ります。バグ報告・要望等あればこちらまでお願いします。')
+        await message.channel.send('このBotのヘルプです。\n\n**「t.con (オプション：反応する対象、言語)」**\n(使用例：t.con en server)\n自分が接続しているVCにBotを接続させます。\n反応する対象：\n・指定なし(もしくはchannel)･･･コマンドのチャンネルに反応\n・server･･･サーバー全体に反応\n\n言語：\n・指定なし(もしくはjp)･･･日本語\n・en･･･英語\n・kr･･･韓国語\n・ch･･･中国語\n\n**「t.dc」**\n自分が接続しているVCからこのBotを切断します。\n\n**「t.expand (オプション：on/off)」\nリンク展開機能のオンオフを切り替えます。**\n\n**「t.release note」**\nこのBotの最新のアップデート内容を確認できます。\n\n**「t.invite」**\nこのBotの招待リンクを送ります。ご自由にお使い下さい。\n\n**「t.support」**\nこのBotのサポートサーバーの招待リンクを送ります。バグ報告・要望等あればこちらまでお願いします。')
+        return
     if message.content == 't.release note':
         await message.channel.send('◆2020/07/14(22:07)リリース◆\n\n機能追加\n・サポートサーバーへのリンクを追加\n\nバグフィックス\n・なし')
+        return
     if message.content == 't.invite':
         await message.channel.send('このBotの招待リンクです。導入してもらえると喜びます。\n開発者:Alpaca#8032\nhttps://discord.com/api/oauth2/authorize?client_id=727508841368911943&permissions=3153472&scope=bot')
+        return
     if message.content == 't.support':
         await message.channel.send('このBotのサポートサーバーです。バグ報告・要望等あればこちらにお願いします。お気軽にどうぞ。\nhttps://discord.gg/DbtZAcX')
+        return
+    if message.content.startswith('t.expand')
+        if message.content[9:11] == 'on'
+            if message.guild.id in expand_off:
+                expand_off.remove(message.guild.id)
+                with open('expand.json', 'w', encoding='utf-8') as f:
+                    json.dump(expand_off, f, ensure_ascii=False, indent=4)
+                filepath = 'expand.json'
+                title = 'expand.json'
+                file = drive.CreateFile(
+                    {'id': '1zX-mbDeN_Mlx-p_62WSE5zAgsqu_jFX5', 'title': title, 'mimeType': 'application/json'})
+                file.SetContentFile(filepath)
+                file.Upload()
+                print('upload-complete')
+                await message.channel.send('メッセージ展開をオンにしました。')
+                return
+            else:
+                await message.channel.send('メッセージ展開は既にオンです。')
+                return
+
+        if message.content[9:12] == 'off'
+            if message.guild.id not in expand_off:
+                expand_off.append(message.guild.id)
+                with open('expand.json', 'w', encoding='utf-8') as f:
+                    json.dump(expand_off, f, ensure_ascii=False, indent=4)
+                filepath = 'expand.json'
+                title = 'expand.json'
+                file = drive.CreateFile(
+                    {'id': '1zX-mbDeN_Mlx-p_62WSE5zAgsqu_jFX5', 'title': title, 'mimeType': 'application/json'})
+                file.SetContentFile(filepath)
+                file.Upload()
+                print('upload-complete')
+                await message.channel.send('メッセージ展開をオフにしました。')
+                return
+            else:
+                await message.channel.send('メッセージ展開は既にオフです。')
+                return
 
     if message.guild is None:
         if message.author.id == 539910964724891719:
             if message.content == 'サーバー':
                 await message.channel.send(str(len(client.guilds)))
+                return
     if message.content.startswith('t.con'):
         global voice_active
         if message.author.voice is None:
