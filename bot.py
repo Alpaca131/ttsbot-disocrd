1d3ffc4d66b6a99cd616d99dff475a9f77a38bfe
BuildTools <unconfigured@null.spigotmc.org>
2020-07-20 00:52:38 +0900

    added english help

--- bot.py
+++ bot.py
@@ -56,9 +56,13 @@ async def on_message(message):
     if message.content == 't.help':
         await message.channel.send('このBotのヘルプです。\n\n**「t.con (オプション：反応する対象、言語、文字数制限)」**\n(使用例：t.con lang=en server limit=50)\n自分が接続しているVCにBotを接続させます。\n\n反応する対象：\n・指定なし(もしくはchannel)･･･コマンドのチャンネルに反応\n・server･･･サーバー全体に反応\n\n文字数制限(limit=文字数)：\n・反応する文字数を制限できます。\n\n言語(lang=)：\n・指定なし(もしくはjp)･･･日本語\n・en･･･英語\n・kr･･･韓国語\n・ch･･･中国語\n・auto･･･自動(※遅延が増加する場合があります。)\n\n**「t.dc」**\n自分が接続しているVCからこのBotを切断します。\n\n**「t.expand (オプション：on/off)」**\nリンク展開機能のオンオフを切り替えます。\n\n**「t.release note」**\nこのBotの最新のアップデート内容を確認できます。\n\n**「t.invite」**\nこのBotの招待リンクを送ります。ご自由にお使い下さい。\n\n**「t.support」**\nこのBotのサポートサーバーの招待リンクを送ります。バグ報告・要望等あればこちらまでお願いします。')
         return
+    if message.content == 't.help en':
+        await message.channel.send('Wirting now...\n・ω・')
     if message.content == 't.release note':
         await message.channel.send('◆2020/07/19(09:51)リリース◆\n\n機能追加\n・なし\n\nバグフィックス\n・サーバー、チャンネルの選択が出来なくなっていた不具合を修正')
         return
+    if message.content == 't.release note en':
+        await message.channel.send('◆2020/07/19(09:51)Released◆\n\nAdded function\n・None\n\nBug fix\n・Fixed channel or server select')
     if message.content == 't.invite':
         await message.channel.send('このBotの招待リンクです。導入してもらえると喜びます。\n開発者:Alpaca#8032\nhttps://discord.com/api/oauth2/authorize?client_id=727508841368911943&permissions=3153472&scope=bot')
         return
